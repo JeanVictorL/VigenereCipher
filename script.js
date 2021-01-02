c546cb972a9a8496b850886cb1e51ddba3025867
ShizaanSil <jeanvictor.lopes@gmail.com>
2021-01-02 00:41:43 -0300

    made the input field

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let key = "bustling"
-let ptext = document.getElementById("ptext").innerHTML
-let cipher = document.getElementById("cipher").innerHTML
+let key = document.getElementById("key")
+let ptext = document.getElementById("ptext")
+let cipher = document.getElementById("cipher")
+let form = document.getElementById("myForm")
 
 function makeKey() {
     let nkey = []
 
     for (let i = 0;; i++) {
-        if (i == key.length) {
+        if (i == key.value.length) {
             i = 0
         }
-        if (nkey.length == ptext.length) {
+        if (nkey.length == ptext.value.length) {
             break
         }
-        nkey.push(key[i])
+        nkey.push(key.value[i])
     }
     return nkey
 }
@@ -20,15 +21,15 @@ function makeKey() {
 function encrypt() {
     let encText = ""
     let k = makeKey()
-    for (let i = 0; i < ptext.length; i++) {
-        if (ptext[i].charCodeAt(0) < 97 || ptext[i].charCodeAt(0) > 123) {
+    for (let i = 0; i < ptext.value.length; i++) {
+        if (ptext.value[i].charCodeAt(0) < 97 || ptext.value[i].charCodeAt(0) > 123) {
             encText += ""
             k.splice(i, 0, "")
-            if (ptext[i] == " ") {
+            if (ptext.value[i] == " ") {
                 encText += " "
             }
         } else {
-            let x = ((ptext.charCodeAt(i) - 97) + (k[i].charCodeAt(0) - 97)) % 26
+            let x = ((ptext.value.charCodeAt(i) - 97) + (k[i].charCodeAt(0) - 97)) % 26
             x = String.fromCharCode(x + 97)
             encText += x
         }
@@ -40,7 +41,7 @@ function encrypt() {
 function decrypt() {
     let npText = ""
     let k = makeKey()
-    let c = encrypt()
+    let c = cipher.value
     for (let i = 0; i < c.length; i++) {
         if (c[i].charCodeAt(0) < 97 || c[i].charCodeAt(0) > 123) {
             npText += ""
@@ -57,5 +58,10 @@ function decrypt() {
     return npText
 }
 
-document.getElementById("cipher").innerHTML = encrypt()
-document.getElementById("decrypted").innerHTML = decrypt()
+ptext.oninput = () => {
+    cipher.value = encrypt()
+}
+
+cipher.oninput = () => {
+    ptext.value = decrypt()
+}
