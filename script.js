e5eb82b7fced326246be6264aa7a03839b9371c4
ShizaanSil <jeanvictor.lopes@gmail.com>
2021-01-02 15:03:01 -0300

    altered the formatting

--- script.js
+++ script.js
@@ -1,67 +1,72 @@
-let key = document.getElementById("key")
-let ptext = document.getElementById("ptext")
-let cipher = document.getElementById("cipher")
-let form = document.getElementById("myForm")
+let key = document.getElementById("key");
+let ptext = document.getElementById("ptext");
+let cipher = document.getElementById("cipher");
+let form = document.getElementById("myForm");
 
 function makeKey() {
-    let nkey = []
+	let nkey = [];
 
-    for (let i = 0;; i++) {
-        if (i == key.value.length) {
-            i = 0
-        }
-        if (nkey.length == ptext.value.length) {
-            break
-        }
-        nkey.push(key.value[i])
-    }
-    return nkey
+	for (let i = 0; ; i++) {
+		if (i == key.value.length) {
+			i = 0;
+		}
+		if (nkey.length == ptext.value.length) {
+			break;
+		}
+		nkey.push(key.value[i]);
+	}
+	return nkey;
 }
 
 function encrypt() {
-    let encText = ""
-    let k = makeKey()
-    for (let i = 0; i < ptext.value.length; i++) {
-        if (ptext.value[i].charCodeAt(0) < 97 || ptext.value[i].charCodeAt(0) > 123) {
-            encText += ""
-            k.splice(i, 0, "")
-            if (ptext.value[i] == " ") {
-                encText += " "
-            }
-        } else {
-            let x = ((ptext.value.charCodeAt(i) - 97) + (k[i].charCodeAt(0) - 97)) % 26
-            x = String.fromCharCode(x + 97)
-            encText += x
-        }
-
-    }
-    return encText
+	let encText = "";
+	let k = makeKey();
+	for (let i = 0; i < ptext.value.length; i++) {
+		if (
+			ptext.value[i].charCodeAt(0) < 97 ||
+			ptext.value[i].charCodeAt(0) > 123
+		) {
+			encText += "";
+			k.splice(i, 0, "");
+			if (ptext.value[i] == " ") {
+				encText += " ";
+			}
+		} else {
+			let x =
+				(ptext.value.charCodeAt(i) - 97 + (k[i].charCodeAt(0) - 97)) %
+				26;
+			x = String.fromCharCode(x + 97);
+			encText += x;
+		}
+	}
+	return encText;
 }
 
 function decrypt() {
-    let npText = ""
-    let k = makeKey()
-    let c = cipher.value
-    for (let i = 0; i < c.length; i++) {
-        if (c[i].charCodeAt(0) < 97 || c[i].charCodeAt(0) > 123) {
-            npText += ""
-            k.splice(i, 0, "")
-            if (c[i] == " ") {
-                npText += " "
-            }
-        } else {
-            let x = ((c.charCodeAt(i) - 97) - (k[i].charCodeAt(0) - 97) + 26) % 26
-            x = String.fromCharCode(x + 97)
-            npText += x
-        }
-    }
-    return npText
+	let npText = "";
+	let k = makeKey();
+	let c = cipher.value;
+	for (let i = 0; i < c.length; i++) {
+		if (c[i].charCodeAt(0) < 97 || c[i].charCodeAt(0) > 123) {
+			npText += "";
+			k.splice(i, 0, "");
+			if (c[i] == " ") {
+				npText += " ";
+			}
+		} else {
+			let x =
+				(c.charCodeAt(i) - 97 - (k[i].charCodeAt(0) - 97) + 26) % 26;
+			x = String.fromCharCode(x + 97);
+			npText += x;
+		}
+	}
+	return npText;
 }
 
 ptext.oninput = () => {
-    cipher.value = encrypt()
-}
+	cipher.value = encrypt();
+};
 
 cipher.oninput = () => {
-    ptext.value = decrypt()
-}
+	ptext.value = decrypt();
+};
